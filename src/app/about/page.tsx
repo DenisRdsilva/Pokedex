3a5dd95429a01dfb434f8d132c452ad740aafb9f
[Denis} <[den_ou@hotmail.com]>
2024-05-05 13:31:00 -0300

    Atualizando imagens

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,7 +11,7 @@ export default function About() {
         <Text textAlign={'center'} fontSize={'22px'}>I'm a huge fan of the pokemon franchise and I wanna become a Front-End Developer, so I decided to make my own pokedex for practicing and that's how this project was born. I used Next.JS and Chakra UI</Text>
       </Flex>
       <Center>
-        <Image src='/grimm.png' w='400px' />
+        <Image src='https://archives.bulbagarden.net/media/upload/b/ba/HOME0861.png' w='400px' />
       </Center>
     </Flex>
   )
--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
         <Flex position='fixed' direction={'row'} bg='black' w='100%' h='60px' zIndex={'11'}>
             <Flex pl='35'>
                 <Center>
-                    <Image src='/pokeball.png' w='27' h='27' />
+                    <Image src='https://e7.pngegg.com/pngimages/324/645/png-clipart-pokeball-pokeball-thumbnail.png' w='27' h='27' />
                     <Text color='white' pl='5' fontSize={'22px'} mt='15' mb='15'>Pokedex</Text>
                 </Center>
             </Flex>
