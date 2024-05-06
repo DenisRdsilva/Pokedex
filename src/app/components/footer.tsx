a01775ae3a43fee22935bae33cc5d59f0e5c30a6
[Denis} <[den_ou@hotmail.com]>
2024-05-06 01:17:03 -0300

    Edição no rodapé

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -3,7 +3,7 @@ import { Flex, Text } from '@chakra-ui/react'
 export default function Footer() {
     return (
         <Flex position='fixed' bottom={'0'} bg='black' w='100%' h="50px" zIndex={'5'} justifyContent="center" alignItems="center">
-            <Text fontSize={'18px'} color='white'>Pokedex &copy; 2023 Designed by Dsilva</Text>
+            <Text fontSize={'18px'} color='white'>Pokedex &copy; DSilva</Text>
         </Flex>
     )
 }
